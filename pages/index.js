77b456855e2f400d0f33917c070ab82c12db47ae
= <denemehesabi351@gmail.com>
2021-05-10 18:07:45 +0300

    unimportant update

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,15 @@ export default function Home() {
           <h1>first post</h1>
           <p>Uber’de engineering manager (mühendis müdürü) olarak çalışan Dan Heller’ın kendi blogunda ‘Ten Principles for Growth as an Engineer’ başlığıyla paylaştığı yazıyı çok etkili buldum. Kendim defalarca okudum, sizlerinde bundan faydalanmasını istiyorum. Eğer ingilizcenin yeteri seviyedeyse orjinalini okumanızı tavsiye ediyorum.
 Yazıyı beğendiğiniz ve faydalı bulduğunuz takdirde takım arkadaşlarınızla, sizinle çalışan mühendislerle paylaşmanızı öneririm. Bir mühendis çoğu zaman takımının ortalamasından daha ileriye gidemiyor. Etrafınızdakilerin gelişimine katkı sağlamak uzun vadede sizi de ileriye götürecek, bunu unutmamak lazım. Son olarak yazının aslına sadık kalmaya çalıştım, fakat bazı noktalarda kelime seçiminde insiyatif kullandım. İyi okumalar.</p>
+          <h5 className="date">10 May 2021</h5>
         </div>
       </div>
 
       <style jsx>{`
+        .date{
+          text-align: right;
+          color: #c7c4c1;
+        }
         .container{
           width: 650px;
           max-width: 100%;
